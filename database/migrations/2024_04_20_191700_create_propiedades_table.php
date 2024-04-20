dba109e1fd222dc45306c581931501e60a952e7a
Zemog gomez <zemogmjg@gmail.com>
2024-04-20 14:57:33 -0500

    Migraciones de las 3 bd

---
+++ database/migrations/2024_04_20_191700_create_propiedades_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('propiedades', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('dirección');
+            $table->enum('tipo', ['casa', 'apartamento', 'local comercial']);
+            $table->integer('tamaño');
+            $table->integer('numero_habitaciones');
+            $table->string('precio');
+            $table->enum('estado', ['disponible', 'vendida', 'arrendada']);
+    
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('propiedades');
+    }
+};
---
+++ database/migrations/2024_04_20_195331_create_transacciones_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transacciones', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('propiedad_id');
+            $table->foreign('propiedad_id')->references('id')->on('propiedades');
+
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+
+            $table->enum('tipo_transacción', ['venta', 'compra', 'arrendamiento']);
+            $table->date('fecha_transacción');
+            $table->integer('monto_transacción');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transacciones');
+    }
+};
